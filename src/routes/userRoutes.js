0725768a6201a2a17610221e1adec87d3952a274
S-M-J-I <jishanlion@gmail.com>
2023-08-20 17:20:13 +0600

    added verificaiton apis

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,12 +4,24 @@ const router = express.Router()
 const user_controllers = require('../controllers/userController')
 
 const multer = require('multer')
-const uploadAvatar = multer({ dest: 'images/avatar' })
+const uploadAvatar = multer({
+    dest: 'images/avatar',
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname)
+    }
+})
+const uploadVerification = multer({
+    dest: 'verification/',
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname)
+    }
+})
 const { checkAuth } = require('../middlewares/authMiddleware')
 
 router.post('/signup', uploadAvatar.single('avatar'), user_controllers.userSignup)
 router.post('/login', uploadAvatar.single('avatar'), user_controllers.userLogin)
 router.post('/set-account-detail', checkAuth, user_controllers.setAccountDetail)
+router.post('/verification-send/:id', uploadVerification.single('file'), user_controllers.saveVerification)
 router.get('/get', checkAuth, user_controllers.fetchUserDetailsApiMethod)
 router.post('/logout', checkAuth, user_controllers.userLogout)
 
